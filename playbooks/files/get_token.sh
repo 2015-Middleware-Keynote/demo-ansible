a25ba24204867b83096ef7f818e9e92073f56213
Jason DeTiberus <jdetiber@redhat.com>
2015-07-27 16:17:20 -0400

    Add support for deploying modified hexboard by default

--- playbooks/files/get_token.sh
+++ playbooks/files/get_token.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -eu
-osc config view --flatten -o template -t '{{with index .users 0}}{{.user.token}}{{end}}'
+oc config view --flatten -o template -t '{{with index .users 0}}{{.user.token}}{{end}}'
