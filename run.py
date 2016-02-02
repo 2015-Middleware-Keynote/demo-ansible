aad7ba67d2e24a00a89f3311d2da4dae866ab22f
Siamak Sadeghianfar <ssadeghi@redhat.com>
2016-02-02 16:53:54 +0100

    fixed a smoke test bug

--- run.py
+++ run.py
@@ -51,10 +51,7 @@ env_sizes = {'tiny': 1,
 @click.option('--run-smoke-tests', is_flag=True, help='Run workshop smoke tests')
 @click.option('--num-smoke-test-users', default=5, type=int,
               help='Number of smoke test users', show_default=True)
-<<<<<<< HEAD
 @click.option('--run-only-smoke-tests', is_flag=True, help='Run only the workshop smoke tests')
-=======
->>>>>>> db2b9e950c6bf8575ddb8036d1d5f10b13216801
 @click.option('--default-password', default='openshift3',
               help='password for all users', show_default=True)
 @click.help_option('--help', '-h')
@@ -67,7 +64,8 @@ def launch_demo_env(env_size=None, region=None, ami=None, no_confirm=False,
                     deployment_type=None, console_port=443, api_port=443,
                     rhsm_user=None, rhsm_pass=None,
                     skip_subscription_management=False, run_smoke_tests=False,
-                    num_smoke_test_users=None, default_password=None, verbose=0):
+                    num_smoke_test_users=None, run_only_smoke_tests=False,
+                    default_password=None, verbose=0):
     click.echo('Configured values:')
     click.echo('\tcluster_id: %s' % cluster_id)
     click.echo('\tenv_size: %s' % env_size)
@@ -92,7 +90,7 @@ def launch_demo_env(env_size=None, region=None, ami=None, no_confirm=False,
     click.echo('Application wildcard zone for this env will be %s' % wildcard_zone)
 
 
-    if run_smoke_tests:
+    if run_smoke_tests or run_only_smoke_tests:
         click.echo('Smoke tests will be run following environment creation with %s users with password %s' % (num_smoke_test_users, default_password))
 
     if run_only_smoke_tests:
@@ -114,7 +112,7 @@ def launch_demo_env(env_size=None, region=None, ami=None, no_confirm=False,
     if run_only_smoke_tests:
         playbook = 'playbooks/projects_setup.yml'
 
-    command='ansible-playbook -i inventory/aws/hosts -e \'cluster_id=%s ec2_region=%s ec2_image=%s ec2_keypair=%s ec2_master_instance_type=%s ec2_infra_instance_type=%s ec2_node_instance_type=%s r53_zone=%s r53_host_zone=%s r53_wildcard_zone=%s num_app_nodes=%s hexboard_size=%s deployment_type=%s api_port=%s console_port=%s rhsm_user=%s rhsm_pass=%s skip_subscription_management=%s run_smoke_tests=%s num_smoke_test_users=%s default_password=%s\' %s' % (cluster_id, region, ami, keypair, master_instance_type, infra_instance_type, node_instance_type, r53_zone, host_zone, wildcard_zone, env_sizes[env_size], env_size, deployment_type, api_port, console_port, rhsm_user, rhsm_pass, skip_subscription_management, run_smoke_tests, num_smoke_test_users, default_password, playbook)
+    command='ansible-playbook -i inventory/aws/hosts -e \'cluster_id=%s ec2_region=%s ec2_image=%s ec2_keypair=%s ec2_master_instance_type=%s ec2_infra_instance_type=%s ec2_node_instance_type=%s r53_zone=%s r53_host_zone=%s r53_wildcard_zone=%s num_app_nodes=%s hexboard_size=%s deployment_type=%s api_port=%s console_port=%s rhsm_user=%s rhsm_pass=%s skip_subscription_management=%s run_smoke_tests=%s run_only_smoke_tests=%s num_smoke_test_users=%s default_password=%s\' %s' % (cluster_id, region, ami, keypair, master_instance_type, infra_instance_type, node_instance_type, r53_zone, host_zone, wildcard_zone, env_sizes[env_size], env_size, deployment_type, api_port, console_port, rhsm_user, rhsm_pass, skip_subscription_management, run_smoke_tests, run_only_smoke_tests, num_smoke_test_users, default_password, playbook)
 
     if verbose > 0:
         command += " -" + "".join(['v']*verbose)
