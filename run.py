ee668090ea6c942c2f3510f2f632c12c9589935e
Erik M Jacobs <erikmjacobs@gmail.com>
2016-02-25 13:55:49 -0500

    - separated api and console port - it seemed to be at one point - adds a name for the vpc to better identify it - forces the destination filename for the certificates - didn't need any of the project setup cruft since we only are interacting with first_master - don't want smoke projects in main cluster setup - makes project setup work by finding the first master and adding to a special group - changes the ec2 cache to only display if verbose is set - tweaked hexboard size output - Several changes to openshift_setup to facilitate master DNS entry   * switches from deprecated "sudo: no" syntax to "become: no"   * removes some ansible_ssh / ansible_sudo statements that were unnecessary and angered the dns play   * adds a play for creating the DNS entry for the first master, useful for SSH into the master (since you can't SSH through the loadbalancer) - disable installation of cockpit - tweaks play name and breaks demo project into its own play - move group setting tasks to separate file for reuse in other plays - switch to become: no to be current with ansible best practice - Several cleanup changes for the various setups.   * Switches to using the "centralized" group setter   * removes stale group setters   * and so on - Modifications to post_setup to make it isolated   * Uses new group tasks   * Determines service IP from master and then writes it to nodes - Two bug fixes for the post setup   * Fixes an error in the kube service determination   * Fixes the missing default context in the demo play - Cleanup and a bugfix   * Moves host_repos to main folder (out of util)   * un-comments the openshift_sdn_mtu which is required for 3.1.1.6 which has broken MTU detection - moves register host to main playbooks folder - moves post_setup to main playbook area with required changes - hexboard size override now works - Various cleanup and debug changes   * pretties up the ansible-playbook command to make it more readable and easier to add options   * adds a --debug-playbook flag so that you can run a single, specific playbook and get all the     "right" options applied to Ansible without having to do it manually - Improves speed and changes repo   * No need to gather facts for host_repos or register_host   * Changes the currently used mirror - 3.1.1.6-4 fixes the MTU issues so the MTU setting is no longer required - configures cloudformation template dependencies so that delete is successful - Tweaks and cleanup of run.py   * Changes subscription managmeent to only prompt when not skipped   * Moves around items and adds some comments for clarity and readability   * Don't display RHSM information if registration is skipped   * Adds various sanity checks and reformats (confirmation) output - Cleanup enhancements and other tweaks   * refactors confirmation to use click's built-in abort mechanism   * moves cleanup into core run script as an option   * various tweaks and enhancements to prompts and other logic - More cleanup and refactoring   * Combines subscription management and repo management into a single playbook   * Creates a "cleanup" play that does the aws config and always updates systems     related to bz: https://bugzilla.redhat.com/show_bug.cgi?id=1317031 - correctly set api port and add supporting parameter for cloudformation template - force master count to three - forcing the openshift package version - fixed an issue with cleanup not finding any hosts - Removed the unnecessary disk setup from the master cloudformation user data. - Added some confirmation output after all runs are finished.

--- run.py
+++ run.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python
+# vim: sw=2 ts=2
 
 import click
 import os
 import sys
 
-def validate_masters(ctx, param, value):
-    if value == 2:
-        raise click.BadParameter('num-masters must be 1 or 3')
-    return value
-
 hexboard_sizes = ['tiny', 'xsmall', 'small', 'medium', 'large', 'xlarge']
 
 @click.command()
+
+### Cluster options
 @click.option('--cluster-id', default='demo', show_default=True,
-              help='cluster identifier (used for assigning ec2 tags, naming security groups, and is used as a subdomain of the r53-zone for environment dns entries')
+              help='Cluster identifier (used for prefixing/naming various items created in AWS')
 @click.option('--num-nodes', type=click.INT, default=1, show_default=True,
               help='Number of application nodes')
 @click.option('--num-infra', type=click.IntRange(1,3), default=1,
               show_default=True, help='Number of infrastructure nodes')
-@click.option('--num-masters', type=click.IntRange(1,3), default=1,
-              show_default=True, help='Number of masters',
-              callback=validate_masters)
 @click.option('--hexboard-size', type=click.Choice(hexboard_sizes),
-              help='Hexboard size (nodes tiny=32, xsmall=64, small=108, medium=266, large=512, xlarge=1026)',
+              help='Override Hexboard size calculation (tiny=32, xsmall=64, small=108, medium=266, large=512, xlarge=1026)',
               show_default=True)
+@click.option('--console-port', default='8443', type=click.IntRange(1,65535), help='OpenShift web console port',
+              show_default=True)
+@click.option('--api-port', default='8443', type=click.IntRange(1,65535), help='OpenShift API port',
+              show_default=True)
+@click.option('--deployment-type', default='openshift-enterprise', help='openshift deployment type',
+              show_default=True)
+@click.option('--default-password', default='openshift3',
+              help='password for all users', show_default=True)
+
+### Smoke test options
+@click.option('--run-smoke-tests', is_flag=True, help='Run workshop smoke tests')
+@click.option('--num-smoke-test-users', default=5, type=click.INT,
+              help='Number of smoke test users', show_default=True)
+@click.option('--run-only-smoke-tests', is_flag=True, help='Run only the workshop smoke tests')
+
+### AWS/EC2 options
 @click.option('--region', default='us-east-1', help='ec2 region',
               show_default=True)
-@click.option('--ami', default='ami-2051294a', help='ec2 ami',
+@click.option('--ami', default='ami-10251c7a', help='ec2 ami',
               show_default=True)
 @click.option('--master-instance-type', default='m4.large', help='ec2 instance type',
               show_default=True)
@@ -36,17 +47,16 @@ hexboard_sizes = ['tiny', 'xsmall', 'small', 'medium', 'large', 'xlarge']
               show_default=True)
 @click.option('--keypair', default='default', help='ec2 keypair name',
               show_default=True)
-@click.option('--r53-zone', prompt=True,
-              help='route53 hosted zone (must be pre-configured)')
+
+### DNS options
+@click.option('--r53-zone', help='route53 hosted zone (must be pre-configured)')
 @click.option('--app-dns-prefix', default='apps', help='application dns prefix',
               show_default=True)
-@click.option('--deployment-type', default='openshift-enterprise', help='openshift deployment type',
-              show_default=True)
-@click.option('--console-port', default=443, type=click.IntRange(1,65535),
-              help='OpenShift console/api port')
-@click.option('--rhsm-user', prompt=True, help='Red Hat Subscription Management User')
-@click.option('--rhsm-pass', prompt=True, hide_input=True,
-              help='Red Hat Subscription Management Password')
+
+### Subscription and Software options
+@click.option('--rhsm-user', help='Red Hat Subscription Management User')
+@click.option('--rhsm-pass', help='Red Hat Subscription Management Password',
+                hide_input=True,)
 @click.option('--skip-subscription-management', is_flag=True,
               help='Skip subscription management steps')
 @click.option('--use-certificate-repos', is_flag=True,
@@ -55,100 +65,253 @@ hexboard_sizes = ['tiny', 'xsmall', 'small', 'medium', 'large', 'xlarge']
               show_default=True)
 @click.option('--certificate-key', help='Certificate key for the yum repository',
               show_default=True)
+
+### Miscellaneous options
 @click.option('--no-confirm', is_flag=True,
               help='Skip confirmation prompt')
-@click.option('--run-smoke-tests', is_flag=True, help='Run workshop smoke tests')
-@click.option('--num-smoke-test-users', default=5, type=click.INT,
-              help='Number of smoke test users', show_default=True)
-@click.option('--run-only-smoke-tests', is_flag=True, help='Run only the workshop smoke tests')
-@click.option('--default-password', default='openshift3',
-              help='password for all users', show_default=True)
+@click.option('--debug-playbook',
+              help='Specify a path to a specific playbook to debug with all vars')
+@click.option('--cleanup', is_flag=True,
+              help='Deletes environment')
 @click.help_option('--help', '-h')
 @click.option('-v', '--verbose', count=True)
-def launch_demo_env(num_nodes, num_infra, num_masters, hexboard_size=None,
-                    region=None, ami=None, no_confirm=False,
-                    master_instance_type=None, node_instance_type=None,
-                    infra_instance_type=None, keypair=None, r53_zone=None,
-                    cluster_id=None, app_dns_prefix=None, console_port=443,
-                    deployment_type=None, rhsm_user=None, rhsm_pass=None,
-                    skip_subscription_management=False, run_smoke_tests=False,
-                    num_smoke_test_users=None, run_only_smoke_tests=False,
-                    default_password=None, verbose=0):
-    click.echo('Configured values:')
-    click.echo('\tcluster_id: %s' % cluster_id)
-    click.echo('\tnodes: %s' % num_nodes)
-    click.echo('\tinfra nodes: %s' % num_infra)
-    click.echo('\tmasters: %s' % num_masters)
-    click.echo('\tconsole port: %s' % console_port)
 
-    if hexboard_size is not None:
-        click.echo("hexboard size is: %s" % hexboard_size)
-    elif num_nodes <= 1:
-        hexboard_size = 'tiny'
+def launch_demo_env(num_nodes,
+                    num_infra,
+                    hexboard_size=None,
+                    region=None,
+                    ami=None,
+                    no_confirm=False,
+                    master_instance_type=None,
+                    node_instance_type=None,
+                    infra_instance_type=None,
+                    keypair=None,
+                    r53_zone=None,
+                    cluster_id=None,
+                    app_dns_prefix=None,
+                    deployment_type=None,
+                    console_port=443,
+                    api_port=443,
+                    rhsm_user=None,
+                    rhsm_pass=None,
+                    skip_subscription_management=False,
+                    certificate_file=None,
+                    certificate_key=None,
+                    use_certificate_repos=False,
+                    run_smoke_tests=False,
+                    num_smoke_test_users=None,
+                    run_only_smoke_tests=False,
+                    default_password=None,
+                    debug_playbook=None,
+                    cleanup=False,
+                    verbose=0):
+
+  # Force num_masters = 3 because of an issue with API startup and ELB health checks and more
+  num_masters = 3
+
+  # If not running cleanup need to prompt for the R53 zone:
+  if r53_zone is None:
+    r53_zone = click.prompt('R53 zone')
+
+  # Cannot run cleanup with no-confirm
+  if cleanup and no_confirm:
+    click.echo('Cannot use --cleanup and --no-confirm as it is not safe.')
+
+  # If skipping subscription management, must have cert repos enabled
+  # If cleaning up, this is ok
+  if not cleanup:
+    if skip_subscription_management and not use_certificate_repos:
+      click.echo('Cannot skip subscription management without using certificate repos.')
+      sys.exit(1)
+
+  # If using subscription management, cannot use certificate repos
+  if not skip_subscription_management and use_certificate_repos:
+    click.echo('Must skip subscription management when using certificate repos')
+    sys.exit(1)
+
+  # Prompt for RHSM user and password if not skipping subscription management
+  if not skip_subscription_management:
+    # If the user already provided values, don't bother asking again
+    if rhsm_user is None:
+      rhsm_user = click.prompt("RHSM username?")
+    if rhsm_pass is None:
+      rhsm_pass = click.prompt("RHSM password?", hide_input=True, confirmation_prompt=True)
+
+  # Prompt for certificate files if using certicicate repos
+  if use_certificate_repos and not cleanup:
+    if certificate_file is None:
+      certificate_file = click.prompt("Certificate file absolute location? (eg: /home/user/folder/file.pem)")
+    if certificate_key is None:
+      certificate_key = click.prompt("Certificate key absolute location? (eg: /home/user/folder/file.pem)")
+
+  # Override hexboard size calculation
+  if hexboard_size is None:
+    if num_nodes <= 1:
+      hexboard_size = 'tiny'
     elif num_nodes < 3:
-        hexboard_size = 'xsmall'
+      hexboard_size = 'xsmall'
     elif num_nodes < 5:
-        hexboard_size = 'small'
+      hexboard_size = 'small'
     elif num_nodes < 9:
-        hexboard_size = 'medium'
+      hexboard_size = 'medium'
     elif num_nodes < 15:
-        hexboard_size = 'large'
+      hexboard_size = 'large'
     else:
-        hexboard_size = 'xlarge'
-    click.echo('\thexboard_size: %s' % hexboard_size)
-    click.echo('\tregion: %s' % region)
-    click.echo('\tami: %s' % ami)
-    click.echo('\tmaster instance_type: %s' % master_instance_type)
-    click.echo('\tnode_instance_type: %s' % node_instance_type)
-    click.echo('\tinfra_instance_type: %s' % infra_instance_type)
-    click.echo('\tkeypair: %s' % keypair)
-    click.echo('\tr53_zone: %s' % r53_zone)
-    click.echo('\tapp_dns_prefix: %s' % app_dns_prefix)
-    click.echo('\tdeployment_type: %s' % deployment_type)
+      hexboard_size = 'xlarge'
+
+  # Calculate various DNS values
+  host_zone="%s.%s" % (cluster_id, r53_zone)
+  wildcard_zone="%s.%s.%s" % (app_dns_prefix, cluster_id, r53_zone)
+
+  # Display information to the user about their choices
+  click.echo('Configured values:')
+  click.echo('\tcluster_id: %s' % cluster_id)
+  click.echo('\tami: %s' % ami)
+  click.echo('\tregion: %s' % region)
+  click.echo('\tmaster instance_type: %s' % master_instance_type)
+  click.echo('\tnode_instance_type: %s' % node_instance_type)
+  click.echo('\tinfra_instance_type: %s' % infra_instance_type)
+  click.echo('\tkeypair: %s' % keypair)
+  click.echo('\tnodes: %s' % num_nodes)
+  click.echo('\tinfra nodes: %s' % num_infra)
+  click.echo('\tmasters: %s' % num_masters)
+  click.echo('\tconsole port: %s' % console_port)
+  click.echo('\tapi port: %s' % api_port)
+  click.echo('\tdeployment_type: %s' % deployment_type)
+  click.echo('\thexboard_size: %s' % hexboard_size)
+  click.echo('\tr53_zone: %s' % r53_zone)
+  click.echo('\tapp_dns_prefix: %s' % app_dns_prefix)
+  click.echo('\thost dns: %s' % host_zone)
+  click.echo('\tapps dns: %s' % wildcard_zone)
+
+  # Don't bother to display subscription manager values if we're skipping subscription management
+  if not skip_subscription_management:
     click.echo('\trhsm_user: %s' % rhsm_user)
     click.echo('\trhsm_pass: *******')
 
-    host_zone="%s.%s" % (cluster_id, r53_zone)
-    wildcard_zone="%s.%s.%s" % (app_dns_prefix, cluster_id, r53_zone)
+  if use_certificate_repos:
+    click.echo('\tcertificate_file: %s' % certificate_file)
+    click.echo('\tcertificate_key: %s' % certificate_key)
 
-    click.echo('Host DNS entries will be created under the %s domain' % host_zone)
-    click.echo('Application wildcard zone for this env will be %s' % wildcard_zone)
+  if run_smoke_tests or run_only_smoke_tests:
+    click.echo('\tnum smoke users: %s' % num_smoke_test_users)
 
-    if use_certificate_repos:
-        click.echo('Certificate file %s and certificate key %s will be used for the yum repos' % (certificate_file, certificate_key))
+  click.echo('\tdefault password: %s' % default_password)
 
-    if run_smoke_tests or run_only_smoke_tests:
-        click.echo('Smoke tests will be run following environment creation with %s users with password %s' % (num_smoke_test_users, default_password))
+  click.echo("")
 
-    if run_only_smoke_tests:
-        click.echo('Only smoke tests will be run.')
+  if run_only_smoke_tests:
+    click.echo('Only smoke tests will be run.')
 
-    if not no_confirm and not click.confirm('Continue using these values?'):
-        sys.exit(0)
+  if debug_playbook:
+    click.echo('We will debug the following playbook: %s' % (debug_playbook))
 
-    if run_only_smoke_tests:
-        playbooks = ['projects_setup.yaml']
-    else:
-        playbooks = ['bootstrap.yml', 'openshift_setup.yml', 'projects_setup.yaml']
+  if not no_confirm and not cleanup:
+    click.confirm('Continue using these values?', abort=True)
 
-    for playbook in playbooks:
-        # refresh the inventory cache to prevent stale hosts from
-        # interferring with re-running
-        command='inventory/aws/hosts/ec2.py --refresh-cache'
-        os.system(command)
+  # Special confirmations for cleanup
+  if cleanup:
+    click.confirm('Delete the cluster %s' % cluster_id, abort=True)
+    click.confirm('ARE YOU REALLY SURE YOU WANT TO DELETE THE CLUSTER %s' % cluster_id, abort=True)
+    click.confirm('Press enter to continue', abort=True, default=True)
 
-        # remove any cached facts to prevent stale data during a re-run
-        command='rm -rf .ansible/cached_facts'
-        os.system(command)
+  if debug_playbook:
+    playbooks = [debug_playbook]
+  elif run_only_smoke_tests:
+    playbooks = ['playbooks/projects_setup.yml']
+  elif cleanup:
+    playbooks = ['playbooks/cleanup.yml']
+  else:
+    playbooks = ['playbooks/bootstrap.yml', 'playbooks/openshift_setup.yml', 'playbooks/projects_setup.yml']
 
-        command='ansible-playbook -i inventory/aws/hosts -e \'cluster_id=%s ec2_region=%s ec2_image=%s ec2_keypair=%s ec2_master_instance_type=%s ec2_infra_instance_type=%s ec2_node_instance_type=%s r53_zone=%s r53_host_zone=%s r53_wildcard_zone=%s num_app_nodes=%s num_infra_nodes=%s console_port=%s num_masters=%s hexboard_size=%s deployment_type=%s rhsm_user=%s rhsm_pass=%s skip_subscription_management=%s run_smoke_tests=%s num_smoke_test_users=%s default_password=%s\' playbooks/%s' % (cluster_id, region, ami, keypair, master_instance_type, infra_instance_type, node_instance_type, r53_zone, host_zone, wildcard_zone, num_nodes, num_infra, console_port, num_masters, hexboard_size, deployment_type, rhsm_user, rhsm_pass, skip_subscription_management, run_smoke_tests, num_smoke_test_users, default_password, playbook)
+  for playbook in playbooks:
 
-        if verbose > 0:
-            command += " -" + "".join(['v']*verbose)
+    # hide cache output unless in verbose mode
+    devnull='> /dev/null'
 
-        status = os.system(command)
-        if os.WIFEXITED(status) and os.WEXITSTATUS(status) != 0:
-            return os.WEXITSTATUS(status)
+    if verbose > 0:
+      devnull=''
+
+    # refresh the inventory cache to prevent stale hosts from
+    # interferring with re-running
+    command='inventory/aws/hosts/ec2.py --refresh-cache %s' % (devnull)
+    os.system(command)
+
+    # remove any cached facts to prevent stale data during a re-run
+    command='rm -rf .ansible/cached_facts'
+    os.system(command)
+
+    command='ansible-playbook -i inventory/aws/hosts -e \'cluster_id=%s \
+    ec2_region=%s \
+    ec2_image=%s \
+    ec2_keypair=%s \
+    ec2_master_instance_type=%s \
+    ec2_infra_instance_type=%s \
+    ec2_node_instance_type=%s \
+    r53_zone=%s \
+    r53_host_zone=%s \
+    r53_wildcard_zone=%s \
+    console_port=%s \
+    api_port=%s \
+    num_app_nodes=%s \
+    num_infra_nodes=%s \
+    num_masters=%s \
+    hexboard_size=%s \
+    deployment_type=%s \
+    rhsm_user=%s \
+    rhsm_pass=%s \
+    skip_subscription_management=%s \
+    use_certificate_repos=%s \
+    certificate_file=%s \
+    certificate_key=%s \
+    run_smoke_tests=%s \
+    run_only_smoke_tests=%s \
+    num_smoke_test_users=%s \
+    default_password=%s\' %s' % (cluster_id,
+                    region,
+                    ami,
+                    keypair,
+                    master_instance_type,
+                    infra_instance_type,
+                    node_instance_type,
+                    r53_zone,
+                    host_zone,
+                    wildcard_zone,
+                    console_port,
+                    api_port,
+                    num_nodes,
+                    num_infra,
+                    num_masters,
+                    hexboard_size,
+                    deployment_type,
+                    rhsm_user,
+                    rhsm_pass,
+                    skip_subscription_management,
+                    use_certificate_repos,
+                    certificate_file,
+                    certificate_key,
+                    run_smoke_tests,
+                    run_only_smoke_tests,
+                    num_smoke_test_users,
+                    default_password,
+                    playbook)
+
+
+    if verbose > 0:
+      command += " -" + "".join(['v']*verbose)
+
+    status = os.system(command)
+    if os.WIFEXITED(status) and os.WEXITSTATUS(status) != 0:
+      return os.WEXITSTATUS(status)
+
+  # if the last run playbook didn't explode, assume cluster provisioned successfully
+  # but make sure that user was not just running tests or cleaning up
+  if os.WIFEXITED(status) and os.WEXITSTATUS(status) == 0:
+    if not debug_playbook and not run_only_smoke_tests and not cleanup:
+      click.echo('Your cluster provisioned successfully. The console is available at https://openshift.%s:%s' % (host_zone, console_port)) 
+
+    if cleanup:
+      click.echo('Your cluster, %s, was de-provisioned and removed successfully.' % (cluster_id))
 
 if __name__ == '__main__':
-    launch_demo_env(auto_envvar_prefix='OSE_DEMO')
+  launch_demo_env(auto_envvar_prefix='OSE_DEMO')
